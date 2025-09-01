12cef680f6cb0d358a1f8999ada3b9b856a7b139
Darshana Sangokar <137633544+DarshanaSangokar@users.noreply.github.com>
2025-09-01 19:28:51 +0530

    Update PatientService.java

--- springboot-hospital-crud/src/main/java/com/code/service/PatientService.java
+++ springboot-hospital-crud/src/main/java/com/code/service/PatientService.java
@@ -21,32 +21,8 @@ public class PatientService {
 	public List<Patient> getAllPatients(){
 		return patientRepo.findAll();
 	}
-	/**********/
-	// delete
-	public ResponseEntity<Patient> deletePatient(Long patientId) {
-		Patient patient =patientRepo.findById(patientId).get();
-		patientRepo.deleteById(patientId);
-		return ResponseEntity.ok(patient);
-	}
-
-	public ResponseEntity<Patient> updatePatient(long id, Patient patient) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
-	/// THIS MIGHT BE NOT SUITABLE I GUESS DOCTOR IS REQUIRED:: HERE
-	public Patient createPatient(Patient patient) {
-		// TODO Auto-generated method stub
-		return patientRepo.save(patient);
-	}
-
-	// I HAVE TO IMPLEMENT THAT::
-	
-	//GET ALL PATIENTS BY DOCTORS
-	//GET PATIENT BY ID
-	// UPDATE PATIENT
-	// DELETE PATIENT
-	// ADD NEW PATIENT
 	
 	
 }
+
